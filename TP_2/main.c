1531db847ccc4238be2cf2ad2ff0a9be30466fce
talibe LOQ <omarba2015@gmail.com>
2024-11-26 15:42:16 +0100

    starting TP 2

--- TP_2/main.c
+++ TP_2/main.c
@@ -9,10 +9,26 @@ main.c
 
 int main(void){
 
-    char *texte = "abracadabra";
-    char *motif = "abra";
-    int occ = algo_naif_boucle_rapide_sans_santinelle(texte, motif);
-    printf("algo_naif_boucle_rapide_sans_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ);
-    printf("algo_naif_boucle_rapide_avec_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ);
+    // char *texte = "abracadabra";
+    // char *motif = "abra";
+    // int occ_1 = algo_naif_boucle_rapide_sans_santinelle(texte, motif);
+    // int occ_2 = algo_naif_boucle_rapide_avec_santinelle(texte, motif);
+    // int occ_3 = algo_naif_sans_boucle_rapide_sans_santinelle(texte, motif);
+    // int occ_4 = algo_naif_strncomp_sans_boucle_rapide_sans_santinelle(texte, motif);
+    // int occ_5 = algo_naif_strncomp_avec_boucle_rapide_sans_santinelle(texte, motif);
+    // int occ_6 = algo_naif_strncomp_avec_boucle_rapide_sans_santinelle(texte, motif);
+    // printf("algo_naif_boucle_rapide_sans_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ_1);
+    // printf("algo_naif_boucle_rapide_avec_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ_2);
+    // printf("algo_naif_sans_boucle_rapide_sans_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ_3);
+    // printf("algo_naif_sans_boucle_rapide_sans_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ_3);
+    // printf("algo_naif_strncomp_sans_boucle_rapide_sans_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ_4);
+    // printf("algo_naif_strncomp_avec_boucle_rapide_sans_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ_5);
+    // printf("algo_naif_strncomp_avec_boucle_rapide_avec_santinelle : occ  de %s dans %s : %d\n", motif, texte, occ_6);
+
+    // char *texte = "abracadabra";
+    char *motif = "aabraa";
+    printf("prefix : %d\n", fonction_prefix(motif, 2));
+
+
     return 0;
 }
--- TP_2/tp2.c
+++ TP_2/tp2.c
@@ -11,9 +11,10 @@ int algo_naif_boucle_rapide_sans_santinelle(char *texte, char *motif){
     int taille_texte = strlen(texte);
     int taille_motif = strlen(motif);
     int count = 0;
+    char c = motif[0];  
     while(i < taille_texte){
 
-        if (texte[i] == motif[0]){
+        if (texte[i] == c){
             int j = 1;
             while(j < taille_motif && texte[i+j] == motif[j]){
                 j++;
@@ -35,15 +36,16 @@ int algo_naif_boucle_rapide_avec_santinelle(char *texte, char *motif){
     char *texte_santinelle = (char *) malloc(taille_texte + taille_motif + 1);
     strcpy(texte_santinelle, texte);
     strcat(texte_santinelle, motif);
+    char c = motif[0];
     while(1){
 
-        if (texte_santinelle[i] == motif[0]){
+        if (texte_santinelle[i] == c){
             int j = 1;
-            while(j < taille_motif && texte[i+j] == motif[j]){
+            while(j < taille_motif && texte_santinelle[i+j] == motif[j]){
                 j++;
             }
             if (j == taille_motif){
-                if( i + taille_motif == taille_texte){
+                if( i == taille_texte){
                     return count;
                 }
                 count ++;
@@ -52,4 +54,97 @@ int algo_naif_boucle_rapide_avec_santinelle(char *texte, char *motif){
         i++;
     }
     return -1;
+}
+
+
+int algo_naif_sans_boucle_rapide_sans_santinelle(char *texte, char *motif){
+    int i = 0;
+    int count = 0;
+    int taille_texte = strlen(texte);
+    int taille_motif = strlen(motif);
+
+    while(i < taille_texte - taille_motif + 1){
+        int j = 0;
+        while(j < taille_motif && texte[i+j] == motif[j]){
+            j++;
+        }
+        if (j == taille_motif){
+            count ++;
+        }
+        i++;
+    }
+    return count;
+}
+
+
+int algo_naif_strncomp_sans_boucle_rapide_sans_santinelle(char *texte, char *motif){
+    int i = 0;
+    int count = 0;
+    int taille_texte = strlen(texte);
+    int taille_motif = strlen(motif);
+
+    while(i < taille_texte - taille_motif + 1){
+        if (strncmp(texte + i, motif, taille_motif) == 0){
+            count ++;
+        }
+        i++;
+    }
+    return count;
+}
+
+int algo_naif_strncomp_avec_boucle_rapide_sans_santinelle(char *texte, char *motif){
+    int i = 0;
+    int count = 0;
+    int taille_texte = strlen(texte);
+    int taille_motif = strlen(motif);
+    char c = motif[0];
+    while(i < taille_texte - taille_motif + 1){
+
+         if (texte[i] == c){
+            if (strncmp(texte + i , motif, taille_motif) == 0){
+                count ++;
+            }
+        } 
+        i++;
+    }
+    return count;
+}
+
+int algo_naif_strncomp_avec_boucle_rapide_avec_santinelle(char *texte, char *motif){
+    int i = 0;
+    int count = 0;
+    int taille_texte = strlen(texte);
+    int taille_motif = strlen(motif);
+    char *texte_santinelle = (char *) malloc(taille_texte + taille_motif + 1);
+    strcpy(texte_santinelle, texte);
+    strcat(texte_santinelle, motif);
+    char c = motif[0];
+    while(1){
+        if (texte_santinelle[i] == c){
+            if (strncmp(texte_santinelle + i , motif, taille_motif) == 0){
+                if( i == taille_texte){
+                    return count;
+                }
+                count ++;
+            }
+        }
+        i++;
+    }
+    return -1;
+}
+
+
+int fonction_prefix(char *motif, int i){
+    int j = 0;
+    int periode = 0;
+    while(j < i){
+        if (motif[i] == motif[j]){
+            return j + 1;
+        }
+    }
+    return j;
+}
+
+int algo_morris_pratt(char *texte, char *motif){
+    int table_prefix [strlen(motif)];
 }
